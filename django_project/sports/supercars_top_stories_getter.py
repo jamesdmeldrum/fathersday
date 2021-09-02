86d774880843044978a23c05e7ffd097d6ef14ec
jamesdmeldrum <62580013+jamesdmeldrum@users.noreply.github.com>
2021-09-02 20:03:28 +1000

    fix

--- django_project/sports/supercars_top_stories_getter.py
+++ django_project/sports/supercars_top_stories_getter.py
@@ -4,12 +4,13 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    url = "https://au.motorsport.com/v8supercars/news"
+    url = "https://au.motorsport.com/v8supercars/news/"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     x = soup.find_all("div", class_="ms-item--art")
     top = x[0].find_all("a")[0]
-    link = url + top.get("href")
+    link = "https://au.motorsport.com" + top.get("href")
+    print(link)
     title = top.get("title")
 
     dict = {
